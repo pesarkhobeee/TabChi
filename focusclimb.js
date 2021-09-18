2b545e84058a6d094eb438d193bcb471cbabbe66
Farid Ahmadian <ahmadian.farid.1988@gmail.com>
2021-09-19 01:27:35 +0200

    feat(clock): add the base clock

--- focusclimb.js
+++ focusclimb.js
@@ -120,3 +120,36 @@ window.addEventListener("keydown", function (event) {
       return; // Quit when this doesn't handle the key event.
   }
 }, true);
+
+$(document).ready(function() {
+  clockUpdate();
+  setInterval(clockUpdate, 1000);
+})
+
+function clockUpdate() {
+  var date = new Date();
+  $('.digital-clock').css({'color': '#fff', 'text-shadow': '0 0 6px #ff0'});
+  function addZero(x) {
+    if (x < 10) {
+      return x = '0' + x;
+    } else {
+      return x;
+    }
+  }
+
+  function twelveHour(x) {
+    if (x > 12) {
+      return x = x - 12;
+    } else if (x == 0) {
+      return x = 12;
+    } else {
+      return x;
+    }
+  }
+
+  var h = addZero(twelveHour(date.getHours()));
+  var m = addZero(date.getMinutes());
+  var s = addZero(date.getSeconds());
+
+  $('.digital-clock').text(h + ':' + m)
+}
