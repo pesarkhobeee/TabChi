2b9efcde4972ca5ac4bfd1c44915c75823cbe9b9
pesarkhobeee <ahmadian.farid.1988@gmail.com>
2023-04-10 08:15:30 +0200

    fix: change digital clock css style

--- clock.js
+++ clock.js
@@ -1,6 +1,5 @@
 function clockUpdate() {
     var date = new Date();
-    $('#digital-clock').css({'color': '#fff', 'text-shadow': '0 0 4px rgba(0,0,0,0.8)'});
     function addZero(x) {
       if (x < 10) {
         return x = '0' + x;
